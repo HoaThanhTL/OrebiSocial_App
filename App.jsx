769bcd220947028e669ea4b6afd8282c66c17277
HoaThanhTL <vominhthuc01051993@gmail.com>
2025-05-07 08:52:40 +0700

    OrebiSocialApp Start Login Signup

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppNavigator from "./src/navigation/AppNavigator";
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import { store } from "./src/app/store";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </Provider>
+    </GestureHandlerRootView>
+  );
+}
